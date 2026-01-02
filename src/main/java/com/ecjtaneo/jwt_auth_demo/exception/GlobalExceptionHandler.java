04248300d456a8ec8a9758fdb629f7f26646b6f3
EcjTn <ecjtaneo@gmail.com>
2026-01-02 12:57:33 +0800

    added custom exceptions and handlers

---
+++ src/main/java/com/ecjtaneo/jwt_auth_demo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.ecjtaneo.jwt_auth_demo.exception;
+
+import com.ecjtaneo.jwt_auth_demo.dto.response.MessageResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceConflictException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public MessageResponse handleConflict(RuntimeException ex) {
+        return new MessageResponse(ex.getMessage());
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public MessageResponse handleNotFound(RuntimeException ex) {
+        return new MessageResponse(ex.getMessage());
+    }
+
+    @ExceptionHandler({HttpMessageNotReadableException.class, MethodArgumentNotValidException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public MessageResponse handleBadRequest(Exception ex) {
+        return new MessageResponse("Request body is missing or invalid");
+    }
+
+}
