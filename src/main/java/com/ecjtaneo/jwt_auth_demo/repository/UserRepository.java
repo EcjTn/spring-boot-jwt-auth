0be25078859b2ca8ad0e7c4e939b84154703c1df
EcjTn <ecjtaneo@gmail.com>
2026-01-02 22:06:41 +0800

    fixed username find

--- src/main/java/com/ecjtaneo/jwt_auth_demo/repository/UserRepository.java
+++ src/main/java/com/ecjtaneo/jwt_auth_demo/repository/UserRepository.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     public boolean existsByUsername(String username);
-    public Optional<User> findByUsername();
+    public Optional<User> findByUsername(String username);
 }
