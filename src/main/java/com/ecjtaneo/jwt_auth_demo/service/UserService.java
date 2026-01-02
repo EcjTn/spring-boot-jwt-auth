6425aec0f2eee9c40e99bc2338e85a2b3f9ac860
EcjTn <ecjtaneo@gmail.com>
2026-01-02 12:35:00 +0800

    added user register

--- src/main/java/com/ecjtaneo/jwt_auth_demo/service/UserService.java
+++ src/main/java/com/ecjtaneo/jwt_auth_demo/service/UserService.java
@@ -1,5 +1,8 @@
 package com.ecjtaneo.jwt_auth_demo.service;
 
+import com.ecjtaneo.jwt_auth_demo.dto.request.UserRegisterDto;
+import com.ecjtaneo.jwt_auth_demo.dto.response.MessageResponse;
+import com.ecjtaneo.jwt_auth_demo.model.User;
 import com.ecjtaneo.jwt_auth_demo.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +12,14 @@ public class UserService {
 
     public UserService(UserRepository userRepo) {
         this.userRepo = userRepo;
+    }
+
+    public MessageResponse add(UserRegisterDto dto) {
+        User user = new User();
+        user.setUsername(dto.username());
+        user.setPassword(dto.password());
+
+        return new MessageResponse("Successfully registered.");
     }
 
 }
