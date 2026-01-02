c640e09d8c4e603319da25e245fef365680ba140
EcjTn <ecjtaneo@gmail.com>
2026-01-02 12:14:31 +0800

    Initial commit

---
+++ src/main/java/com/ecjtaneo/jwt_auth_demo/model/User.java
@@ -0,0 +1,22 @@
+package com.ecjtaneo.jwt_auth_demo.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(nullable = false)
+    String username;
+
+    @Column(nullable = false)
+    String password;
+    LocalDateTime created_at = LocalDateTime.now();
+}
---
+++ src/main/java/com/ecjtaneo/jwt_auth_demo/service/JwtService.java
@@ -0,0 +1,49 @@
+package com.ecjtaneo.jwt_auth_demo.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    @Value("${spring.jwt.xd}")
+    private String jwtKey;
+    private final Date expiry = new Date(System.currentTimeMillis() + 15 * 60 * 1000); //15 mins
+    private SecretKey secretKey;
+
+    @PostConstruct
+    public void init() {
+        this.secretKey = Keys.hmacShaKeyFor(jwtKey.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generate(String sub) {
+        return Jwts
+                .builder()
+                .signWith(secretKey)
+                .subject(sub)
+                .expiration(expiry)
+                .compact();
+    }
+
+    public Claims validate(String token) {
+        return Jwts
+                .parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String extractSubject(Claims claims) {
+        return claims.getSubject();
+    }
+
+}
