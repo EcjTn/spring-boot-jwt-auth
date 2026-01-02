f7286a16599d942cd94ec42a915d15e6d1ec6b57
EcjTn <ecjtaneo@gmail.com>
2026-01-02 22:03:54 +0800

    fixed user checks

--- src/main/java/com/ecjtaneo/jwt_auth_demo/repository/UserRepository.java
+++ src/main/java/com/ecjtaneo/jwt_auth_demo/repository/UserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    public boolean existsByUsername();
+    public boolean existsByUsername(String username);
     public Optional<User> findByUsername();
 }
