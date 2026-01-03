cada89ce63a6a31b882965358c3fdaf29149ee9d
EcjTn <ecjtaneo@gmail.com>
2026-01-03 13:35:20 +0800

    fixed password setting

--- src/main/java/com/ecjtaneo/jwt_auth_demo/service/UserService.java
+++ src/main/java/com/ecjtaneo/jwt_auth_demo/service/UserService.java
@@ -5,18 +5,22 @@ import com.ecjtaneo.jwt_auth_demo.dto.response.MessageResponse;
 import com.ecjtaneo.jwt_auth_demo.mapper.UserMapper;
 import com.ecjtaneo.jwt_auth_demo.model.User;
 import com.ecjtaneo.jwt_auth_demo.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private UserRepository userRepo;
+    private PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepo) {
+    public UserService(UserRepository userRepo, PasswordEncoder passwordEncoder) {
         this.userRepo = userRepo;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public MessageResponse add(UserRegisterDto dto) {
         User user = UserMapper.toEntity(dto);
+        user.setUsername(passwordEncoder.encode(user.getPassword()));
         userRepo.save(user);
 
         return new MessageResponse("Successfully registered.");
