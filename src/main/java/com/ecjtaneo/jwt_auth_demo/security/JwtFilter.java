f1f634db2e88fcfe412fd2f2d2b22239b24830bd
EcjTn <ecjtaneo@gmail.com>
2026-01-03 13:20:51 +0800

    added jwt filter

---
+++ src/main/java/com/ecjtaneo/jwt_auth_demo/security/JwtFilter.java
@@ -0,0 +1,51 @@
+package com.ecjtaneo.jwt_auth_demo.security;
+
+import com.ecjtaneo.jwt_auth_demo.service.JwtService;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+    private JwtService jwtService;
+
+    public JwtFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain filter) throws ServletException, IOException {
+        String authorizationHeader = req.getHeader("Authorization");
+        if(authorizationHeader == null || !authorizationHeader.startsWith("Bearer ") || SecurityContextHolder.getContext().getAuthentication() != null) {
+            filter.doFilter(req, res);
+            return;
+        }
+
+        String jwt = authorizationHeader.substring(7);
+        String userId;
+
+        try {
+            Claims claims = jwtService.validate(jwt);
+            userId = jwtService.extractSubject(claims);
+        } catch (Exception e) {
+            res.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(userId, null, Collections.emptyList());
+        SecurityContextHolder.getContext().setAuthentication(auth);
+        filter.doFilter(req, res);
+
+    }
+}
