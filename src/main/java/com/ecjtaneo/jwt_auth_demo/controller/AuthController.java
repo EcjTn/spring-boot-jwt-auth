14a4ee545f23dae19cf9932306222220811f4eae
EcjTn <ecjtaneo@gmail.com>
2026-01-03 14:47:12 +0800

    added auth register controller

--- src/main/java/com/ecjtaneo/jwt_auth_demo/controller/AuthController.java
+++ src/main/java/com/ecjtaneo/jwt_auth_demo/controller/AuthController.java
@@ -1,11 +1,27 @@
 package com.ecjtaneo.jwt_auth_demo.controller;
 
+import com.ecjtaneo.jwt_auth_demo.dto.request.UserRegisterDto;
+import com.ecjtaneo.jwt_auth_demo.dto.response.MessageResponse;
+import com.ecjtaneo.jwt_auth_demo.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-public class AuthController {}
-//TODO: add AuthService, make use of jwt filters
-// finish auth controller
-// add spring security configs
+public class AuthController {
+    UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public MessageResponse register(@RequestBody @Valid UserRegisterDto dto) {
+        return userService.add(dto);
+    }
+
+
+}
