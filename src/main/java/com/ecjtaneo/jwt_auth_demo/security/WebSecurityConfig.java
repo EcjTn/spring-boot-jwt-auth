553b7593c90333d684cf95612a10c3387b0997ec
EcjTn <ecjtaneo@gmail.com>
2026-01-03 12:58:17 +0800

    added security filter chain

---
+++ src/main/java/com/ecjtaneo/jwt_auth_demo/security/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.ecjtaneo.jwt_auth_demo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        return http
+                .cors(cors -> {})
+                .csrf(c -> c.disable())
+                .formLogin(f -> f.disable())
+                .logout(l -> l.disable())
+                .httpBasic(h -> h.disable())
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(a -> a
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated())
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
