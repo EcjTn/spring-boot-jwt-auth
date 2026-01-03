ed1ab4d597a6b7230677ac40a584b90673031001
EcjTn <ecjtaneo@gmail.com>
2026-01-03 15:41:38 +0800

    fixed missing annotation

--- src/main/java/com/ecjtaneo/jwt_auth_demo/controller/TestController.java
+++ src/main/java/com/ecjtaneo/jwt_auth_demo/controller/TestController.java
@@ -1,9 +1,11 @@
 package com.ecjtaneo.jwt_auth_demo.controller;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @RequestMapping("/test")
 public class TestController {
     @GetMapping("/wco")
