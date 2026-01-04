f731d1bad97b5489264737ac76543eac686af5e5
EcjTn <ecjtaneo@gmail.com>
2026-01-04 16:16:02 +0800

    added refresh token entity

--- src/main/java/com/ecjtaneo/jwt_auth_demo/controller/AuthController.java
+++ src/main/java/com/ecjtaneo/jwt_auth_demo/controller/AuthController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-    AuthService authService;
+    private AuthService authService;
 
     public AuthController(AuthService authService) {
         this.authService = authService;
--- src/main/java/com/ecjtaneo/jwt_auth_demo/model/RefreshToken.java
+++ src/main/java/com/ecjtaneo/jwt_auth_demo/model/RefreshToken.java
@@ -4,23 +4,22 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Data
 @Entity
-@Table(name = "users")
-public class User {
+@Table(name = "refresh_tokens")
+public class RefreshToken {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
     @Column(nullable = false, unique = true)
-    private String username;
+    private String token;
 
-    @Column(nullable = false)
-    private String password;
     private final LocalDateTime created_at = LocalDateTime.now();
-
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
-    private List<RefreshToken> refreshToken;
+    private final LocalDateTime expires_at = LocalDateTime.now().plusDays(30);
 }
